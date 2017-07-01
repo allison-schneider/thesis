e5b133aa9e6fa650d6d7b8b49090c2255dbfcaea
Allison Schneider <aschneider09@gmail.com>
2017-07-01 18:55:00 -0400

    Add files via upload
    It now generates a plot, but the trajectory looks wrong.

--- force_trajectory.py
+++ force_trajectory.py
@@ -26,6 +26,47 @@ def round_to_grid(float, spacing=0.5):
     spacing -- Distance between grid points"""
     return np.round(float / spacing) * spacing
 
+def spherical_hypotenuse(a, b):
+    """ Given the lengths of two sides of a right triangle on a sphere, 
+    a and b, find the length of the hypotenuse c. 
+    
+    Arguments:
+    a -- Length of first side of the triangle, in meters
+    b -- Length of second side of the triangle, in meters 
+    """
+    earth_radius = 6371e3    # meters
+    c = earth_radius * np.arccos(np.cos(a / earth_radius) 
+    	* np.cos(b / earth_radius))
+    return c
+
+def destination(lat1, lon1, distance, bearing):
+    """ Return the latitude and longitude of a destination point 
+    given a starting latitude and longitude, distance, and bearing.
+    
+    Arguments:
+    lat1 -- Starting latitude in degrees, -90 to 90
+    lon1 -- Starting longitude in degrees, 0 to 360
+    distance -- Distance to travel in meters
+    bearing -- Direction between 0 and 360 degrees, clockwise from true North.
+    """
+    earth_radius = 6371e3
+    angular_distance = distance / earth_radius
+    lat2 = np.degrees(np.arcsin(np.sin(np.radians(lat1)) 
+    	* np.cos(angular_distance) + np.cos(np.radians(lat1)) 
+    	* np.sin(angular_distance) * np.cos(np.radians(bearing))))
+    # Longitude is mod 360 degrees for wrapping around earth
+    lon2 = (lon1 + np.degrees(np.arctan2(np.sin(np.radians(bearing)) 
+    	* np.sin(angular_distance) * np.cos(np.radians(lat1)), 
+        np.cos(angular_distance) - np.sin(np.radians(lat1)) 
+        * np.sin(np.radians(lat2))))) % 360       
+    return lat2, lon2
+
+def compass_bearing(math_bearing):
+	""" Transform a vector angle to a compass bearing."""
+	bearing = (5 * np.pi / 2 - math_bearing) % (2 * np.pi)
+	return bearing
+
+
 def calc_dx_dy(longitude,latitude):
     ''' This definition calculates the distance between grid points that are in
         a latitude/longitude format.
@@ -65,19 +106,14 @@ file = netcdf.netcdf_file(filename, mmap=False)
 vars = file.variables
 file.close()
 
-# Open a file and calculate next latitude and longitude from geostrophic balance
-def next_position_force(position_lat, position_lon, filename):
+def speed_force(lat, lon, filename):
 	file = netcdf.netcdf_file(filename, mmap=False)
 	vars = file.variables
 	file.close()
 
-	# Get closest grid position to starting coordinates
-	grid_lat = round_to_grid(position_lat)
-	grid_lon = round_to_grid(position_lon)    
-
 	# Get indices of latitude and longitude in grid
-	lat_index = np.where(vars['lat'][:] == grid_lat)[0][0]
-	lon_index = np.where(vars['lon'][:] == grid_lon)[0][0]
+	lat_index = np.where(vars['lat'][:] == lat)[0][0]
+	lon_index = np.where(vars['lon'][:] == lon)[0][0]
 
 	# Get geopotential height
 	gh = vars['gh'][0][0]
@@ -97,24 +133,37 @@ def next_position_force(position_lat, position_lon, filename):
 
 	g = 9.806 # ms^-2
 	omega = 7.2921150e-5 # radians per second	
-	u_speed = (-g / (2 * omega * np.sin(np.radians(grid_lat)))) * u_gradient
-	v_speed = (g / (2 * omega * np.sin(np.radians(grid_lat)))) * v_gradient
+	u_speed = (-g / (2 * omega * np.sin(np.radians(lat)))) * u_gradient
+	v_speed = (g / (2 * omega * np.sin(np.radians(lat)))) * v_gradient
+	return u_speed, v_speed
 
-	# print("v accel shape is ", np.shape(v_gradient_grid))
-	# print("u accel shape is ", np.shape(u_gradient_grid))
-	# print("u gradient is ", u_gradient)
-	# print("v gradient is ", v_gradient)
-	print("v speed is", v_speed)
-	print("u speed is", u_speed)
-	# print("phi is ", grid_lat)
-	# print("longitude is ", longitudes)
-	# print("longitude shape is ", np.shape(longitudes))
-	# print("latitude is ", latitudes)
-	# print("latitude shape is ", np.shape(latitudes))
-	#print("length grid shape is ", np.shape(length_grid))
-	return gh
 
-def trajectory_force(lat, lon):
+# Open a file and calculate next latitude and longitude from wind speed
+def next_position(position_lat, position_lon, filename):
+	# Get closest grid position to starting coordinates
+	grid_lat = round_to_grid(position_lat)
+	grid_lon = round_to_grid(position_lon) 
+
+	u_speed, v_speed = speed_force(grid_lat, grid_lon, filename)
+
+	# Get magnitude and direction of wind vector
+	wind_speed = spherical_hypotenuse(u_speed, v_speed)
+	wind_direction = np.arctan2(v_speed, u_speed)
+	wind_vector = np.array([wind_speed, wind_direction])
+
+	# Get displacement using velocity times delta t
+	delta_t = 3 * 60 * 60    # in seconds
+	displacement = wind_speed * delta_t    # in meters
+
+	# Calculate new latitude and longitude
+	# Convert wind bearing to degrees
+	wind_bearing = np.degrees(compass_bearing(wind_direction))
+	new_lat, new_lon = destination(grid_lat, grid_lon, displacement, 
+																wind_bearing)
+	return new_lat, new_lon
+
+# Get trajectory from computed wind fields
+def trajectory(lat, lon):
 	trajectory = np.zeros((81,2))
 	# Initial position
 	# Green building is 42.3603088, -71.0893148
@@ -125,10 +174,27 @@ def trajectory_force(lat, lon):
 	    current_lat = trajectory[i,0]
 	    current_lon = trajectory[i,1]
 	trajectory_lat, trajectory_lon = trajectory[:,0], trajectory[:,1]
-	return trajectory_lat, trajectory_lon	
+	return trajectory_lat, trajectory_lon
+
+def test_plot():
+	lat, lon = float(sys.argv[1]), float(sys.argv[2])
+	trajectory_lat, trajectory_lon = trajectory(lat, lon)
+	# Adjust longitude to -180 to 180 range
+	trajectory_lon = trajectory_lon - 180
+	map = Basemap(projection='ortho',lon_0=-105,lat_0=90,resolution='c')
+	map.drawcoastlines(linewidth=0.25)
+	map.drawcountries(linewidth=0)
+	map.fillcontinents(color='coral',lake_color='aqua', zorder=1)
+	# draw the edge of the map projection region (the projection limb)
+	map.drawmapboundary(fill_color='aqua')
+	map.plot(trajectory_lon, trajectory_lat, latlon=True, zorder=2)
+	#plt.savefig('first_trajectory.svg')
+	filename = "trajectory_"+sys.argv[1]+"_"+sys.argv[2]+".png"
+	plt.savefig(filename)
+	plt.show()		
 
 def main():
-	next_position_force(41,71, "hgt-000.nc")
+	speed_force(41,71, "hgt-000.nc")
 
 if __name__ == "__main__":
-	main()
+	test_plot()
