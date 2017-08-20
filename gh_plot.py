17e0c465822061af539fe057916c801974d77c51
Allison Schneider <aschneider09@gmail.com>
2017-08-19 22:50:59 -0400

    equidistant cylindrical plotting function added to gh_plot.py

--- gh_plot.py
+++ gh_plot.py
@@ -15,38 +15,57 @@ gh = vars["gh"][0][0]
 
 lon_grid, lat_grid = np.meshgrid(lon, lat)
 
-print(gh[0])
+map_type = "cyl"
 
+if map_type == "ortho":
+    m = Basemap(projection="ortho", lon_0=-105, lat_0=42, resolution="c")
+    m.drawcoastlines(linewidth=0.25, zorder=3)
+    m.fillcontinents(color="white", lake_color="white", zorder=0)
+    m.drawmapboundary(fill_color="white", zorder=0)
+    m.contourf(lon_grid, lat_grid, gh, latlon=True, zorder=2)
+    plt.title("Geopotential Height at t=0")
+    plt.show()
 
-# Plot one time value
-# map = Basemap(projection="ortho", lon_0=-105, lat_0=42, resolution="c")
-# #map = Basemap(projection="cyl")
-# map.drawcoastlines(linewidth=0.25, zorder=3)
-# map.fillcontinents(color="white", lake_color="white", zorder=0)
-# map.drawmapboundary(fill_color="white", zorder=0)
-# map.contourf(lon_grid, lat_grid, gh, latlon=True, zorder=2)
-# plt.title("Geopotential Height at t=0")
-# plt.show()
+if map_type == "cyl":
+    # Only works with Python 2, not 3
+    m = Basemap(projection="cyl",llcrnrlat=-90,urcrnrlat=90,
+            llcrnrlon=-180,urcrnrlon=180,resolution='c')
+    m.drawcoastlines(linewidth=0.3, zorder=3)
+    m.fillcontinents(color="white", lake_color="white", zorder=0)
+    m.drawmapboundary(fill_color="white", zorder=0)
+    # draw parallels.
+    parallels = np.arange(-90,90,30.)
+    m.drawparallels(parallels, labels=[1,0,0,0], linewidth=0.25, fontsize=8)
+    # draw meridians
+    meridians = np.arange(0,360.,30.)
+    m.drawmeridians(meridians, labels=[0,0,0,1], linewidth=0.25, fontsize=8)
+    gh = m.pcolormesh(lon_grid, lat_grid, gh, latlon=True, 
+        zorder=2, cmap="plasma")
+    plt.title("Geopotential Height at t=0")
+    cbar = m.colorbar(gh, location='bottom', pad="20%")
+    plt.show()
 
-# Make a movie with all time values
-for i, time in enumerate(np.arange(0,241,3)):
-	filename = 'data/hgt-{:03d}.nc'.format(time)
-	file = netcdf.netcdf_file(filename, mmap=False)
-	vars = file.variables
-	file.close()
+#if map_type == "multicyl"
 
-	# Get geopotential height
-	lat = vars["lat"][:][:]
-	lon = vars["lon"][:][:] % 360
-	gh = vars["gh"][0][0]
+# # Make a movie with all time values
+# for i, time in enumerate(np.arange(0,241,3)):
+#   filename = 'data/hgt-{:03d}.nc'.format(time)
+#   file = netcdf.netcdf_file(filename, mmap=False)
+#   vars = file.variables
+#   file.close()
 
-	lon_grid, lat_grid = np.meshgrid(lon, lat)
-	map = Basemap(projection="ortho", lon_0=-105, lat_0=42, resolution="c")
-	map.drawcoastlines(linewidth=0.25, zorder=3)
-	map.fillcontinents(color="white", lake_color="white", zorder=0)
-	map.drawmapboundary(fill_color="white", zorder=0)
-	map.contourf(lon_grid, lat_grid, gh, latlon=True, zorder=2)
-	title = "Geopotential Height at t={}".format(time)
-	plt.title(title)
-	plt.show(block=False)
-	plt.pause(1.0/60.0)
+#   # Get geopotential height
+#   lat = vars["lat"][:][:]
+#   lon = vars["lon"][:][:] % 360
+#   gh = vars["gh"][0][0]
+
+#   lon_grid, lat_grid = np.meshgrid(lon, lat)
+#   m = Basemap(projection="ortho", lon_0=-105, lat_0=42, resolution="c")
+#   m.drawcoastlines(linewidth=0.25, zorder=3)
+#   m.fillcontinents(color="white", lake_color="white", zorder=0)
+#   m.drawmapboundary(fill_color="white", zorder=0)
+#   m.contourf(lon_grid, lat_grid, gh, latlon=True, zorder=2)
+#   title = "Geopotential Height at t={}".format(time)
+#   plt.title(title)
+#   plt.show(block=False)
+#   plt.pause(1.0/60.0)
